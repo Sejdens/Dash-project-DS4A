6fa6c272001cb127f5d22d487d442c9098ef39df
Sejden <sejden@outlook.com>
2022-06-21 16:20:08 -0500

    Refactor

--- app.py
+++ app.py
@@ -13,30 +13,14 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import json
 import os
+
 ################################################################################################
 # Load the data and create the map
 ################################################################################################
 df = pd.read_csv("BASE_FINAL.csv")
 
-###########
-#Genero P6020
-###########
-dic_P6020={"1":"Male","2": "Female"}
-df.P6020=df.P6020.astype("str").replace(dic_P6020, regex=False)
-# merge.P6020=merge.P6020.astype("str").apply(lambda x: dic_P6020, regex=False)
-genero=df[["P6020","FEX_C"]].groupby('P6020')['FEX_C'].sum().reset_index(name="Total") # con sum para sumar el factor de expancion
-genero
-g = px.bar(x="P6020",  y="Total", data_frame=genero,
-           title=("Distribution by Gender"),
-          #  color='#69b3a2'
-# , estimator=
-)
-# px.xlabel("Gender")
-# px.ylabel("Population")
-# px.xticks((0, 1), ("Male", "Female"))
-
 ################################################################################################
-# Create the app
+# Instance declaration
 ################################################################################################
 app = Dash(
     __name__,
@@ -44,6 +28,19 @@ app = Dash(
     plugins=[dl.plugins.pages],
     meta_tags=[{'name':'viewport', 'content':'width=device-width, initial-scale=1.0'}]
 )
+
+###############
+# Genero P6020
+###############
+dic_P6020={"1":"Male","2": "Female"}
+df.P6020=df.P6020.astype("str").replace(dic_P6020, regex=False)
+genero=df[["P6020","FEX_C"]].groupby('P6020')['FEX_C'].sum().reset_index(name="Total") # con sum para sumar el factor de expancion
+g = px.bar(x="P6020",  y="Total", data_frame=genero,
+           title=("Distribution by Gender"),
+)
+# px.xlabel("Gender")
+# px.ylabel("Population")
+# px.xticks((0, 1), ("Male", "Female"))
 
 ################################################################################################
 # Create Layout
@@ -62,7 +59,7 @@ app.layout = dbc.Row([
     ),
     dbc.Col(
         html.Div([
-            html.H2("asdf", id='title'), #Creates the title of the app
+            html.H2("asdf", id='title'),
             dcc.Graph(figure=g,id='main-figure'),
             dcc.Slider(min=0,max=1,marks={0:'US Map', 1:'Scatter Plot'},value=0,id='fig-slider',),
         ]),
@@ -72,8 +69,10 @@ app.layout = dbc.Row([
 align="baseline",
 justify="center",
 )
-# id="div_principal")
 
-#Initiate the server where the app will work
+################################################################################################
+# Initiate the server where the app will work
+#################################################################################################
+
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0',port='8050', threaded=True)

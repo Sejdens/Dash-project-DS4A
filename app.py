58b1d761cfeae1c26add342f64db88b3f8a5cd3d
Sejden <sejden@outlook.com>
2022-06-25 16:58:01 -0500

    Fix root url as about page

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ app = dash.Dash(
 ################################################################################################
 dropdown_exclude = ["pages.not_found_404", "pages.about", "pages.test"] 
 navbar = dbc.NavbarSimple([
-    dbc.NavItem(dbc.NavLink("About", href=request_path_prefix+"about")),
+    dbc.NavItem(dbc.NavLink("About", href=request_path_prefix)),
     dbc.DropdownMenu(
         [
             dbc.DropdownMenuItem(page["name"], href=page["path"])

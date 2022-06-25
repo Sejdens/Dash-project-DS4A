1f930b62bc2c1781c9273707efd479e68831a13a
Sejden <sejden@outlook.com>
2022-06-25 16:34:17 -0500

    Restore debug mode for testing

--- app.py
+++ app.py
@@ -79,6 +79,8 @@ app.layout = dbc.Container(
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0',port='8050',
-        # debug=True,
+    app.run_server(
+        host='0.0.0.0',
+        port='8050',
+        debug=True,
     )

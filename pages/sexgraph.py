87548311b22eaa0fecbcb379d5c24bfe1138378a
andresslc <andresleyvacuenca@gmail.com>
2022-07-02 16:51:48 -0500

    cambio en el label y untracked archivo grande de csv

--- pages/sexgraph.py
+++ pages/sexgraph.py
@@ -7,18 +7,18 @@ from dash import dcc
 
 register_page(__name__, path="/sexgraph")
 
-merge = pd.read_csv("data/MERGE_FINAL_YEARS.csv")
-merge.P6020 = merge.P6020.astype("str").replace({"1.0":"Male","2.0": "Female"}, regex=False)
-merge["YEAR"] = merge["YEAR"].astype(int)
+df = pd.read_csv("data/MERGE_FINAL_YEARS_FILTER_SEX.csv")
+df.P6020 = df.P6020.astype("str").replace({"1.0":"Male","2.0": "Female"}, regex=False)
+df["YEAR"] = df["YEAR"].astype(int)
 
 sex_bar = px.box(
         x = "P6020",
         y = "Index",
-        data_frame = merge,
+        data_frame = df,
         color = "YEAR",
         boxmode="group",
         title="Index by Gender per Year",
-        labels={ "Sex": "" },
+        labels={ "P6020": "" },
     )
 
 layout = dbc.Container([

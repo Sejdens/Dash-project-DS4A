ca96a53828c827385329b536ea67e808399a4daa
Sejden <sejden@outlook.com>
2022-07-05 18:18:26 -0500

    Style Region
    - Center contents inside card
    - Title to <h2>
    - Include checkboxes inside card
    - Space between checkboxes

--- pages/region.py
+++ pages/region.py
@@ -2,7 +2,7 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from dash_labs.plugins import register_page
-from dash import dcc
+from dash import dcc, html, Input, Output, callback
 
 register_page(__name__, path="/region")
 
@@ -22,13 +22,36 @@ region_bar = px.bar(
 layout = dbc.Container([
     dbc.Row([
         dbc.Col([
+            html.H2("Index by Region"),
+            dcc.Checklist(
+                ['2018', '2019', '2020','2021'],
+                ['2018', '2019', '2020','2021'],
+                inline=True,
+                id="region-checklist",
+                labelClassName="px-2",
+                inputClassName="mx-1",
+            ),
             dcc.Graph(
                 id="region-barplot",
                 figure=region_bar,
                 ),
         ],
         lg=8,
-        className='card',
+        className='card align-items-center',
         ),
     ]),
 ])
+
+@callback(
+    Output("region-barplot", "figure"),
+    Input("region-checklist", "value"))
+def filter_bar(filter):
+    fig = px.bar(
+        data_frame=df[df["YEAR"].isin(filter)],
+        x="REGION",
+        y="Index",
+        color="YEAR",
+        barmode="group",
+        labels={ "REGION": "" },
+    )
+    return fig

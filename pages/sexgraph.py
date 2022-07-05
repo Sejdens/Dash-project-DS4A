978c9d468ba4030ec875bb3a8e10d9d708695370
Sejden <sejden@outlook.com>
2022-07-05 18:35:07 -0500

    Style Gender graph
    - Add h2 title
    - Center contents
    - Remove x label
    - Change "sex" for "gender" in code

--- pages/sexgraph.py
+++ pages/sexgraph.py
@@ -2,34 +2,33 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from dash_labs.plugins import register_page
-from dash import dcc
+from dash import dcc, html
 
-register_page(__name__, path="/sexgraph")
+register_page(__name__, path="/gender")
 
-merge = pd.read_csv("data/MERGE_FINAL_YEARS_FILTER_SEX.csv")
-merge.P6020 = merge.P6020.astype("str").replace({"1.0":"Male","2.0": "Female"}, regex=False)
-merge["YEAR"] = merge["YEAR"].astype(int)
+df = pd.read_csv("data/MERGE_FINAL_YEARS_FILTER_SEX.csv")
+df["YEAR"] = df["YEAR"].astype(int)
 
-sex_bar = px.box(
+gender_box = px.box(
         x = "P6020",
         y = "Index",
-        data_frame = merge,
+        data_frame = df,
         color = "YEAR",
         boxmode="group",
-        title="Index by Gender per Year",
-        labels={ "Sex": "" },
+        labels={ "P6020": "" },
     )
 
 layout = dbc.Container([
     dbc.Row([
         dbc.Col([
+            html.H2("Index by Gender"),
             dcc.Graph(
-                id="sex-barplot",
-                figure=sex_bar,
+                id="gender-boxplot",
+                figure=gender_box,
                 ),
         ],
         lg=8,
-        className='card',
+        className='card align-items-center',
         ),
     ]),
 ])

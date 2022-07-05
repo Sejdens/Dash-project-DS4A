100c1014f472310cdcd87cb6668aceb31134fa9c
Sejden <sejden@outlook.com>
2022-07-05 18:13:49 -0500

    Style Read and Write
    - Center contents inside card
    - Title to <h2>
    - Include checkboxes inside card
    - Space between checkboxes

--- pages/readandwritegraph.py
+++ pages/readandwritegraph.py
@@ -2,7 +2,7 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from dash_labs.plugins import register_page
-from dash import dcc
+from dash import dcc, html, Input, Output, callback
 
 register_page(__name__, path="/readandwritegraph")
 
@@ -15,20 +15,42 @@ readandwrite_bar = px.bar(
         y="Index",
         color="YEAR",
         barmode="group",
-        title="Index by the Ability to Read and Write per Year",
         labels={ "P6160": "" },
     )
 
 layout = dbc.Container([
     dbc.Row([
         dbc.Col([
+            html.H2("Index by the Ability to Read and Write"),
+            dcc.Checklist(
+                ['2018', '2019', '2020','2021'],
+                ['2018', '2019', '2020','2021'],
+                inline=True,
+                id="readwrite-checklist",
+                labelClassName="px-2",
+                inputClassName="mx-1",
+            ),
             dcc.Graph(
-                id="sex-barplot",
+                id="readwrite-barplot",
                 figure=readandwrite_bar,
                 ),
         ],
         lg=8,
-        className='card',
+        className='card align-items-center',
         ),
     ]),
 ])
+
+@callback(
+    Output("readwrite-barplot", "figure"),
+    Input("readwrite-checklist", "value"))
+def filter_bar(filter):
+    fig = px.bar(
+        data_frame=df[df["YEAR"].isin(filter)],
+        x="P6160",
+        y="Index",
+        color="YEAR",
+        barmode="group",
+        labels={ "P6160": "" },
+    )
+    return fig

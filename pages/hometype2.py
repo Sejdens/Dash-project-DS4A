ee347f28368642cce416c3ea1a40999f5d22aa47
Sejden <sejden@outlook.com>
2022-07-05 21:19:09 -0500

    Style Home's Floor Material
    - Center card and contents
    - Add spacing for checkboxes
    - Remove x label
    - Add h2 title
    - Fix slug hometype2 -> home_floor

--- pages/hometype2.py
+++ pages/hometype2.py
@@ -4,7 +4,7 @@ import pandas as pd
 from dash_labs.plugins import register_page
 from dash import dcc,html, Input, Output, callback
 
-register_page(__name__, path="/hometype2")
+register_page(__name__, path="/home_floor")
 
 df = pd.read_csv("data/HOMETYPE_P4015.csv")
 df["YEAR"] = df["YEAR"].astype(str)
@@ -15,43 +15,44 @@ hometype_p4015_bar = px.bar(
         y="Index",
         color="YEAR",
         barmode="group",
-        title="Index by Home Type per Year",
-        labels={ "Home Type": "" },
+        labels={ "P4015": "" },
     )
 
 layout = dbc.Container([
     dbc.Row([
-        html.P("Years:"),
-        dcc.Checklist(
-            ['2018', '2019', '2020','2021'],
-            ['2018', '2019', '2020','2021'],
-            inline=True,
-            id="years-checklist"
-        ),
         dbc.Col([
+            html.H2("Index by Home's Floor Material"),
+            dcc.Checklist(
+                ['2018', '2019', '2020','2021'],
+                ['2018', '2019', '2020','2021'],
+                inline=True,
+                id="P4015-checklist",
+                labelClassName="px-2",
+                inputClassName="mx-1",
+            ),
             dcc.Graph(
                 id="hometype-p4015-barplot",
                 figure=hometype_p4015_bar,
                 ),
         ],
         lg=8,
-        className='card',
+        className='card align-items-center',
         ),
-    ]),
+    ],
+    className="justify-content-center",
+    ),
 ])
 
 @callback(
     Output("hometype-p4015-barplot", "figure"), 
-    Input("years-checklist", "value"))
+    Input("P4015-checklist", "value"))
 def filter_bar(filter):
-##    fig = px.imshow(df[df["YEAR"].isin(filter)])
     fig = px.bar(
         data_frame=df[df["YEAR"].isin(filter)],
         x="P4015",
         y="Index",
         color="YEAR",
         barmode="group",
-        title="Index by Home Type per Year",
-        labels={ "Home Type": "" },
+        labels={ "P4015": "" },
     )
     return fig

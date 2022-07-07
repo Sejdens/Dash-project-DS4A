7b6cbcde01effd2bd694e2c69906f95b35e6b388
Sejden <sejden@outlook.com>
2022-07-07 18:17:14 -0500

    Update Edgar's name

--- pages/team.py
+++ pages/team.py
@@ -10,7 +10,7 @@ david = photocard("david.jpeg", "David Alexander Patarroyo Aponte",
 diana = photocard("diana.jpeg", "Diana Aribel Guzmán",
                   "I am from Cali. I am a proactive and goal-oriented Industrial Engineer. I have been working for the last 5 years in multi-professional environments in the pharmaceutical industry. I love to learn, so I am a passionate for reading.")
 
-edgar = photocard("edgar.jpeg", "Edgar",
+edgar = photocard("edgar.jpeg", "Edgar Dario Monsalve",
                   "I am from Arauca, I am a Statistician, I have experience as a data scientist and business analyst in fraud prevention in the financial sector, my greatest hobbies are riding a bicycle and watching formula 1")
 
 jenny = photocard("jenny.jpeg", "Jenny",
